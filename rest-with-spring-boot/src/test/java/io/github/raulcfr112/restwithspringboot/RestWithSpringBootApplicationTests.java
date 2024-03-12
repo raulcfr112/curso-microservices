c0c4084b76eef549361508ea5fc5c7ec66a98659
Raul <raul.cfr112@gmail.com>
2024-03-12 09:29:31 -0400

    First commit

---
+++ rest-with-spring-boot/src/test/java/io/github/raulcfr112/restwithspringboot/RestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.raulcfr112.restwithspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
