c8c699ca34e8a5a73b059b3e726934d162bbac4e
Raul <raul.cfr112@gmail.com>
2024-03-13 10:42:50 -0400

    Criando um projeto spring cloud config server local.

---
+++ greeting-service/src/main/java/io/github/raulcfr112/greetingservice/controllers/GreetingController.java
@@ -0,0 +1,33 @@
+package io.github.raulcfr112.greetingservice.controllers;
+
+import io.github.raulcfr112.greetingservice.configuration.GreetingConfiguration;
+import io.github.raulcfr112.greetingservice.model.Greeting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "%s, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private GreetingConfiguration configuration;
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(
+            @RequestParam(value = "name",
+            defaultValue = "") String name
+    ){
+
+        if (name.isBlank()) name = configuration.getDefaultValue();
+
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, configuration.getGreeting(), name));
+    }
+
+}
